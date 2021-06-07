7f3634d73c6b737facd1457f3ea4756519793341
Genarito <genarocamele@hotmail.com>
2021-06-07 19:04:07 -0300

    Initial commit

---
+++ src/Translator.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class Translator extends React.Component {
+  /**
+   * Render method.
+   *
+   * @returns Component.
+   */
+  render () {
+    return <h1>Hi</h1>
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Translator } from './Translator'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Translator />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
